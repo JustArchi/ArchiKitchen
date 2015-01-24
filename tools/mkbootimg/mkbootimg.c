18f359cbb9ad823971ac805b82718136ad3c227f
JustArchi <JustArchi@JustArchi.net>
2015-01-24 20:39:08 +0100

    mkbootimg 03905f7e5080c175d711726988ac81a3179e3923

--- tools/mkbootimg/mkbootimg.c
+++ tools/mkbootimg/mkbootimg.c
@@ -160,7 +160,10 @@ int main(int argc, char **argv)
             board = val;
         } else if(!strcmp(arg,"--pagesize")) {
             pagesize = strtoul(val, 0, 10);
-            if ((pagesize != 2048) && (pagesize != 4096) && (pagesize != 8192) && (pagesize != 16384) && (pagesize != 32768) && (pagesize != 65536) && (pagesize != 131072)) {
+            if ((pagesize != 2048) && (pagesize != 4096)
+                && (pagesize != 8192) && (pagesize != 16384)
+                && (pagesize != 32768) && (pagesize != 65536)
+                && (pagesize != 131072)) {
                 fprintf(stderr,"error: unsupported page size %d\n", pagesize);
                 return -1;
             }
